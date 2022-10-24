59b922d54af424038d30fe867105172da056fb50
Jason Zhao <jason@wandb.com>
2022-10-24 16:00:29 -0700

    add more github stuff

--- train.py
+++ train.py
@@ -3,15 +3,23 @@ import random
 import json
 import os
 
-wandb.init(project="test")
-
 config_str = os.environ.get("CONFIG", None)
-
 config = {}
 if config_str is not None:
     config = json.loads(config_str)
 
+is_ci = config.get("CI", None)
+name = None
+if is_ci is not None:
+    gitsha = config["GITHUB_SHA"]
+    name = "github-" + gitsha[:6]
+
+wandb.init(project="test", name=name)
+
+
+
 epochs = config.get("epochs", 100)
+
 
 for i in range(1, epochs):
     wandb.log({"loss": random.uniform(0, 1.0 / i),
